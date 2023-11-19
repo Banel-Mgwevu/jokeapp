4367e169788ff83aa326efb5dd0ce1973966e406
Banel Mgwevu <71422029+Banel-Mgwevu@users.noreply.github.com>
2023-11-19 08:15:55 +0200

    NEW CHANGES

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -13,7 +14,16 @@ class MyApp extends StatelessWidget {
       title: 'Quotes and Jokes',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
+        appBarTheme: AppBarTheme(
+          elevation: 0, // Optional: To remove shadow
+          backgroundColor: Colors.orange, // Change the app bar color
+          centerTitle: true,
+          toolbarTextStyle: TextStyle(
+            fontSize: 24, // Increase the font size
+            fontWeight: FontWeight.bold,
+            color: Colors.white, // Text color
+          ),
+        ),),
       home: MyHomePage(),
     );
   }
@@ -26,50 +36,87 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<dynamic> data = [];
+  bool isLoading = true;
+  late String error;
+  late Timer timer;
 
   Future<void> fetchData() async {
     try {
-      final response = await http.get(Uri.parse('http://YOUR_API_URL/random'));
+      final response = await http
+          .get(Uri.parse('https://jokes-api-ppbv.onrender.com/random'));
+      print(
+          'Response status code: ${response.statusCode}'); // Debugging statement
       if (response.statusCode == 200) {
         setState(() {
           data = [json.decode(response.body)];
+          isLoading = false;
         });
+        startTimer();
       } else {
         throw Exception('Failed to load data');
       }
     } catch (error) {
       print('Error fetching data: $error');
+      setState(() {
+        //    error = 'Failed to fetch data. Please check your internet connection or API URL.';
+        isLoading = false;
+      });
     }
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 10), (timer) {
+      fetchData();
+    });
   }
 
   @override
   void initState() {
     super.initState();
+    error = ''; // Initializing error here
     fetchData();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    timer.cancel(); // Cancel the timer to prevent memory leaks
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Quotes and Jokes'),
-      ),
-      body: data.isEmpty
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Quotes and Jokes  '), // Add emojis or additional text here
+              Icon(Icons.emoji_emotions), // Add emojis or icons
+            ],
+          ),
+        ),      body: isLoading
           ? Center(
               child: CircularProgressIndicator(), // Show loading indicator
             )
-          : ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: EdgeInsets.all(8.0),
-                  child: ListTile(
-                    title: Text(data[index]['text'] ?? ''),
-                    subtitle: Text('Author: ${data[index]['author'] ?? "Unknown"}'),
-                  ),
-                );
-              },
-            ),
+          : error.isNotEmpty
+              ? Center(
+                  child: Text(error), // Show error message
+                )
+              : ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return Center(
+                      child: Card(
+                        margin: EdgeInsets.all(8.0),
+                        child: ListTile(
+                          title: Text(data[index]['text'] ?? ''),
+                          subtitle: Text(
+                              'Author: ${data[index]['author'] ?? "Unknown"}'),
+                        ),
+                      ),
+                    );
+                  },
+                ),
     );
   }
 }
